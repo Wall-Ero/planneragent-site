b4d8084f5dccd2e6ce672f379f684f9fe0b46abd
Walter Leonardo Rinaldi <rinaldi_walter@yahoo.it>
2026-08-17 13:01:33 +0200

    feat(cognition): establish operations cost of waiting

--- planneragent/core/src/index.ts
+++ planneragent/core/src/index.ts
@@ -17,3 +17,4 @@ export * from "./cockpit/operational.cockpit.manifestation.v1";
 export * from "./cockpit/operational.availability.read-model.v1";
 export * from "./industrial/cognition/operations.planned.production.material.feasibility.v1";
 export * from "./industrial/cognition/operations.urgency.v1";
+export * from "./industrial/cognition/operations.cost.of.waiting.v1";
